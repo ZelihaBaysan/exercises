907320cc38072cb7497f09a22b3a9cc6f91f33a3
ZelihaBaysan <zelihabaysan@posta.mu.edu.tr>
2024-03-19 00:23:14 +0300

    softcore-the neighbourhood

---
+++ 0gireneKadarTopla.py
@@ -0,0 +1,8 @@
+toplam=0
+while 1:
+    sayi=int(input("bir sayi girin "))
+    if sayi == 0:
+        break
+    toplam=toplam+sayi
+    
+print(toplam)   
---
+++ sayiTekMiCiftMi.py
@@ -0,0 +1,7 @@
+print("Bir sayı gir lan")
+sayi = int(input())
+
+if sayi % 2 == 0:  
+    print("Sayı çifttir")
+else:  
+    print("Sayı tektir")
